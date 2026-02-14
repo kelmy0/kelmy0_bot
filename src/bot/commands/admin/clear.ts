d86671c5164cae64240198f93040bf3f49ef41b5
kelmy0 <kelmykpant09@gmail.com>
2026-02-14 13:11:44 -0300

    feat(commands:admin): Add nem command purge

---
+++ src/bot/commands/admin/clear.ts
@@ -0,0 +1,47 @@
+import { ChatInputCommandInteraction, PermissionFlagsBits, SlashCommandBuilder } from "discord.js";
+import { Command, Translator } from "../../../types/index.js";
+import { handleCommandError } from "../../../utils/index.js";
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName("purge")
+    .setNameLocalizations({ "pt-BR": "limpar" })
+
+    .setDescription("Delete messages in this channel.")
+    .setDescriptionLocalizations({ "pt-BR": "Limpa mensagens nesse canal." })
+
+    .addIntegerOption((option) =>
+      option
+        .setName("amount")
+        .setNameLocalizations({ "pt-BR": "quantidade" })
+
+        .setDescription("Amount messages to clear.")
+        .setDescriptionLocalizations({ "pt-BR": "Quantidade de mensagens para limpar." })
+        .setMinValue(1)
+        .setMaxValue(100)
+        .setRequired(true),
+    )
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+
+  metadata: {
+    category: "admin",
+    production: true,
+  },
+  async execute(interaction: ChatInputCommandInteraction, t: Translator) {
+    await interaction.deferReply({ flags: "Ephemeral" });
+
+    const rawAmount = interaction.options.getInteger("amount", true);
+    const amount = Math.min(100, Math.max(1, rawAmount));
+
+    try {
+      if (!interaction.channel || interaction.channel.isDMBased()) {
+        throw new Error(t("common.errors.no_channel"));
+      }
+
+      await interaction.channel.bulkDelete(amount, true);
+      await interaction.editReply("🧹✅");
+    } catch (error) {
+      await handleCommandError(interaction, "purge", error, t);
+    }
+  },
+} satisfies Command;
